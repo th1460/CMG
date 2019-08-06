ba0672242d169d0237a7437aef74b44208cf90c0
Thiago Pires <thop100@hotmail.com>
2019-08-06 08:07:57 -0300

    First upload

---
+++ app.R
@@ -0,0 +1,234 @@
+library(shiny)
+library(shinymaterial)
+library(DT)
+library(plotly)
+library(DBI)
+library(dplyr)
+library(glue)
+
+# canal
+
+conn <- dbConnect(RSQLite::SQLite(), dbname = "dados.sqlite")
+
+if(!dbExistsTable(conn, "cenarios")){
+  
+  dbWriteTable(conn, "cenarios", value = tibble(cenario = character(), 
+                                                desc = character(),
+                                                custos = numeric())) 
+  
+}
+
+
+ui <- material_page(
+  
+  material_side_nav(
+    
+    tags$h4("Painel de controle"),
+    material_text_box(input_id = "name", label = "Nome do cenário"),
+    material_number_box(input_id = "fator",
+                        label = "Fator retorno veículo (1 ou 2)",
+                        min_value = 1, 
+                        max_value = 2, 
+                        initial_value = 1),
+    material_number_box(input_id = "dura",
+                        label = "Duração (meses)",
+                        min_value = 1,
+                        max_value = Inf,
+                        initial_value = 1),
+    material_number_box(input_id = "exte",
+                        label = "Extensão (km)",
+                        min_value = 1,
+                        max_value = Inf,
+                        initial_value = 1),
+    material_dropdown(input_id = "porte",
+                      label = "Porte",
+                      choices = c("Pequeno", "Médio", "Grande")),
+    material_dropdown(input_id = "relevo",
+                      label = "Relevo",
+                      choices = c("Plano", "Montanhoso", "Ondulado")),
+    material_dropdown(input_id = "classe",
+                      label = "Classe",
+                      choices = c("0", "I-A", "I-B", "II", "III")),
+    material_dropdown(input_id = "canteiro",
+                      label = "Padrão do canteiro",
+                      choices = c("Permanente", "Provisório")),
+    material_slider(input_id = "pavi_a",
+                    label = "Pavimento tipo A (%)",
+                    min_value = 0,
+                    max_value = 100,
+                    initial_value = 0,
+                    step_size = 1),
+    material_slider(input_id = "pavi_b",
+                    label = "Pavimento tipo B (%)",
+                    min_value = 0,
+                    max_value = 100,
+                    initial_value = 0,
+                    step_size = 1),
+    material_slider(input_id = "pavi_c",
+                    label = "Pavimento tipo C (%)",
+                    min_value = 0,
+                    max_value = 100,
+                    initial_value = 0,
+                    step_size = 1),
+    material_slider(input_id = "pavi_d",
+                    label = "Pavimento tipo D (%)",
+                    min_value = 0,
+                    max_value = 100,
+                    initial_value = 0,
+                    step_size = 1),
+    material_slider(input_id = "pavi_e",
+                    label = "Pavimento tipo E (%)",
+                    min_value = 0,
+                    max_value = 100,
+                    initial_value = 0,
+                    step_size = 1),
+    material_slider(input_id = "pavi_f",
+                    label = "Pavimento tipo F (%)",
+                    min_value = 0,
+                    max_value = 100,
+                    initial_value = 0,
+                    step_size = 1),
+    material_slider(input_id = "pavi_g",
+                    label = "Pavimento tipo G (%)",
+                    min_value = 0,
+                    max_value = 100,
+                    initial_value = 0,
+                    step_size = 1)
+  ),
+  
+  title = "Custo Médio Gerencial",
+  nav_bar_color = "indigo",
+  material_tabs(
+    tabs = c("Resumo" = "resumo",
+             "Cenário" = "cenario")
+  ),
+  
+  material_tab_content(
+    
+    tab_id = "resumo",
+    dataTableOutput("resultado"),
+    tags$br(),
+    plotlyOutput("pieplot"),
+    material_button(input_id = "enviar", label = "Enviar")
+    
+  ),
+  
+  material_tab_content(
+    
+    tab_id = "cenario",
+    dataTableOutput("cenarios"),
+    plotlyOutput("plotcenarios"),
+    material_button(input_id = "limpar", label = "Limpar")
+    
+  )
+  
+)
+
+server <- function(input, output, session) {
+  
+  source("cmg.R")
+  
+  tabela <- reactive({
+    
+    custos <- 
+      cmg(porte = input$porte, 
+          relevo = input$relevo, 
+          classe = input$classe,
+          padrao = input$canteiro,
+          k = input$fator,
+          d = input$dura,
+          et = input$exte,
+          peca = input$pavi_a,
+          pecb = input$pavi_b,
+          pecc = input$pavi_c,
+          pecd = input$pavi_d,
+          pece = input$pavi_e,
+          pecf = input$pavi_f,
+          pecg = input$pavi_g)
+    
+    tabela <- 
+      tibble(cenario = input$name, 
+             desc = c("Mobilização e desmobilização (CM1)", 
+                      "Administração local (CM2)", 
+                      "Canteiro de obras (CM3)", 
+                      "Terraplenagem, drenagem e OAC, obras complementares, sinalização e proteção ambiental (CM4)",
+                      "Pavimentação, aquisição e transporte de material betuminoso  (CM5)",
+                      "Custo médio gerencial  (CMG)"), 
+             custos = c(custos$Parcial$CM1, 
+                        custos$Parcial$CM2, 
+                        custos$Parcial$CM3, 
+                        custos$Parcial$CM4,
+                        custos$Parcial$CM5,
+                        custos$CMG))
+    
+  })
+  
+  output$resultado <- renderDataTable({
+    
+    tabela() %>% 
+      datatable(rownames = FALSE, 
+                colnames = c("Cenário", "Custos", "Valores (R$)"),
+                options = list(dom = "t"))
+    
+  })
+  
+  output$pieplot <- renderPlotly({
+    
+    tabela() %>% 
+      slice(-6) %>% 
+      plot_ly(labels = ~desc,
+              values = ~custos,
+              hoverinfo = "percent+label",
+              type = "pie") %>% 
+      layout(showlegend = FALSE)
+    
+  })
+  
+  observeEvent(input$enviar, {
+    dbWriteTable(conn, "cenarios", value = tabela(), append = TRUE)
+    showNotification("Enviado",
+                     action = a(href = "javascript:location.reload();", "Atualizar tabela")
+    )
+  }, ignoreInit = TRUE)
+  
+  output$cenarios <- renderDataTable({
+    datatable(tbl(conn, "cenarios") %>% as_tibble(), 
+              options = list(dom = "tip", pageLength = 6),
+              colnames = c("Cenário", "Custos", "Valores (R$)"),
+              rownames = FALSE)
+  })
+  
+  output$plotcenarios <- renderPlotly({
+    
+    tbl(conn, "cenarios") %>% 
+      filter(desc != "Custo médio gerencial  (CMG)") %>% 
+      as_tibble() %>% 
+      mutate(desc = factor(desc, levels = c("Mobilização e desmobilização (CM1)", 
+                                            "Administração local (CM2)",
+                                            "Canteiro de obras (CM3)",
+                                            "Terraplenagem, drenagem e OAC, obras complementares, sinalização e proteção ambiental (CM4)",
+                                            "Pavimentação, aquisição e transporte de material betuminoso  (CM5)"))) %>% 
+      ggplot(aes(cenario, custos, fill = desc, text = glue("{desc} <br> Cenário: {cenario} <br> R$ {custos}"))) + 
+      geom_col() +
+      labs(x = "Cenários", y = "Custos (R$)", fill = "") +
+      guides(fill = FALSE) +
+      theme_minimal()
+    
+    ggplotly(tooltip = "text") %>% 
+      layout(showlegend = FALSE)
+    
+  })
+  
+  observeEvent(input$limpar, {
+    dbWriteTable(conn, "cenarios", value = tibble(cenario = character(), 
+                                                  desc = character(),
+                                                  custos = numeric()), 
+                 overwrite = TRUE) 
+    showNotification("Enviado",
+                     action = a(href = "javascript:location.reload();", "Atualizar tabela")
+    )
+  }, ignoreInit = TRUE)
+  
+}
+
+shinyApp(ui = ui, server = server)
---
+++ cmg.R
@@ -0,0 +1,84 @@
+require(readxl)
+require(dplyr)
+
+dcm1 <- read_xlsx("CMG.xlsx", sheet = "CM1")
+dcm2a <- read_xlsx("CMG.xlsx", sheet = "CM2A")
+dcm2b <- read_xlsx("CMG.xlsx", sheet = "CM2B")
+dcm3 <- read_xlsx("CMG.xlsx", sheet = "CM3")
+dcm4 <- read_xlsx("CMG.xlsx", sheet = "CM4")
+dcm5 <- read_xlsx("CMG.xlsx", sheet = "CM5")
+
+cmg <- function(porte, relevo, classe, padrao, pavimento, k, d, et,
+                peca, pecb, pecc, pecd, pece, pecf, pecg){
+  
+  cm1 <- dcm1 %>% filter(PORTE == porte) %>% pull(CUSTO)
+  
+  cm2a <- dcm2a %>% filter(PORTE == porte) %>% pull(CUSTO)
+  
+  cm2b <- dcm2b %>% filter(RELEVO == relevo,
+                           CLASSE == classe) %>% pull(CUSTO)
+  
+  cm3 <- dcm3 %>% filter(PORTE == porte, 
+                         PADRAO == padrao) %>% pull(CUSTO)
+  
+  cm4 <- dcm4 %>% filter(RELEVO == relevo,
+                         CLASSE == classe) %>% pull(CUSTO)
+  
+  cm5a <- dcm5 %>% filter(RELEVO == relevo, 
+                          CLASSE == classe,
+                          PAVIMENTO == "A") %>% pull(CUSTO)
+  cm5b <- dcm5 %>% filter(RELEVO == relevo, 
+                          CLASSE == classe,
+                          PAVIMENTO == "B") %>% pull(CUSTO)
+  cm5c <- dcm5 %>% filter(RELEVO == relevo, 
+                          CLASSE == classe,
+                          PAVIMENTO == "C") %>% pull(CUSTO)
+  cm5d <- dcm5 %>% filter(RELEVO == relevo, 
+                          CLASSE == classe,
+                          PAVIMENTO == "D") %>% pull(CUSTO)
+  cm5e <- dcm5 %>% filter(RELEVO == relevo, 
+                          CLASSE == classe,
+                          PAVIMENTO == "E") %>% pull(CUSTO)
+  cm5f <- dcm5 %>% filter(RELEVO == relevo, 
+                          CLASSE == classe,
+                          PAVIMENTO == "F") %>% pull(CUSTO)
+  cm5g <- dcm5 %>% filter(RELEVO == relevo, 
+                          CLASSE == classe,
+                          PAVIMENTO == "G") %>% pull(CUSTO)
+  
+  eca <- peca * et
+  ecb <- pecb * et
+  ecc <- pecc * et
+  ecd <- pecd * et
+  ece <- pece * et
+  ecf <- pecf * et
+  ecg <- pecg * et
+  
+  parcial <- 
+    list(CM1 = cm1 * k,
+         CM2 = cm2a * d + cm2b * et,
+         CM3 = cm3,
+         CM4 = cm4 * et,
+         CM5 = cm5a * eca + cm5b * ecb + cm5c * ecc + cm5d * ecd + cm5e * ece + cm5f * ecf + cm5g * ecg)
+  
+  CMG <- 
+    parcial[[1]] + parcial[[2]] + parcial[[3]] + parcial[[4]] + parcial[[5]]
+  
+  return(list(Parcial = parcial, CMG = CMG))
+  
+}
+
+custos <- cmg(porte = "Pequeno", 
+    relevo = "Ondulado", 
+    classe = "I-A",
+    padrao = "Provisório",
+    k = 1,
+    d = 36,
+    et = 40,
+    peca = .7,
+    pecb = .3,
+    pecc = 0,
+    pecd = 0,
+    pece = 0,
+    pecf = 0,
+    pecg = 0)
